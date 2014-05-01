67f536229eb1b911707623ca574612523dbab785
Padraig Gleeson <p.gleeson@gmail.com>
2014-05-01 12:38:56 +0100

    Adding a python script

---
+++ python/CheckModelDB.py
@@ -0,0 +1,27 @@
+'''
+Script to check ModelDB references
+'''
+
+# From https://github.com/OpenSourceBrain/OSB_API/tree/master/python 
+import osb
+
+projects = 0
+with_modeldb = 0
+    
+for project in osb.get_projects(min_curation_level="None"):
+
+    if project.is_standard_project():
+        
+        print "\n--------   Project: %s\n" % project.name
+        projects+=1
+
+        if project.modeldb_reference:
+            print "    OSB link:%shttp://opensourcebrain.org/projects/%s" % (" "*22, project.identifier)
+            print("    ModelDB link:%shttp://senselab.med.yale.edu/ModelDB/ShowModel.asp?model=%s" % (' '*18, project.modeldb_reference))
+            with_modeldb +=1
+        else:
+            print "    No ModelDB info for model"
+    
+
+print("\nThere were %i projects, %i of which had ModelDB information\n"%(projects, with_modeldb))
+        
---
+++ python/README.md
@@ -0,0 +1,7 @@
+Some Python scripts for interacting with ModelDB & OSB
+========================================= 
+
+CheckModelDB.py
+--------------
+
+A script from https://github.com/OpenSourceBrain/OSB_API/tree/master/python to check ModelDB fields on OSB models
